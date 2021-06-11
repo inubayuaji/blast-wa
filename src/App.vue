f4dc4cadff6796e91cdaa4f9f452fe88e569235d
inubayuaji <inubayuaji@gmail.com>
2021-06-11 10:12:01 +0700

    load excel

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
           :rules="rule.pesan"
           outlined
           label="Pesan"
+          prepend-inner-icon="mdi-comment"
+          color="success"
         ></v-textarea>
         <v-file-input
           v-model="form.kontak"
@@ -16,8 +18,11 @@
           prepend-inner-icon="mdi-microsoft-excel"
           label="Daftar no"
           accept=".xlsx"
+          color="success"
         ></v-file-input>
-        <v-btn color="success" @click="kirim">Kirim</v-btn>
+        <v-btn color="success" @click="kirim"
+          ><v-icon left> mdi-email-send-outline </v-icon> Kirim</v-btn
+        >
       </v-form>
     </v-main>
     <v-footer> Made by <v-btn text color="success">Lunantu</v-btn> </v-footer>
@@ -25,6 +30,9 @@
 </template>
 
 <script>
+import Excel from "exceljs";
+import { parsePhoneNumber, isPossiblePhoneNumber } from "libphonenumber-js";
+
 export default {
   name: "App",
   data() {
@@ -49,6 +57,7 @@ export default {
           },
         ],
       },
+      kontakList: [],
     };
   },
   methods: {
@@ -56,6 +65,39 @@ export default {
       if (!this.$refs.form.validate()) {
         return false;
       }
+
+      this.loadKontak();
+    },
+    async loadKontak() {
+      const workbook = new Excel.Workbook();
+
+      await workbook.xlsx.readFile(this.form.kontak.path);
+      const worksheet = workbook.worksheets[0];
+
+      // load data
+      var excelData = [];
+      worksheet.eachRow({ includeEmpty: true }, function (row) {
+        var cell = row.getCell(1);
+
+        if (cell == "") {
+          excelData.push(null);
+        } else {
+          excelData.push(cell.value.toString());
+        }
+      });
+
+      // normalze data
+      excelData = excelData.filter((phone) => {
+        if (phone != null) {
+          var isPhone = isPossiblePhoneNumber(phone.replace(/\D/g, ""), "ID");
+        }
+        return phone != null && isPhone;
+      });
+      excelData = excelData.map((phone) => {
+        return parsePhoneNumber(phone, "ID").number.replace(/\D/g, "");
+      });
+
+      this.kontakList = excelData;
     },
   },
 };
