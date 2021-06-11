e01ce3d947bc82193c00abf3d55d2edcac96647c
inubayuaji <inubayuaji@gmail.com>
2021-06-11 12:24:39 +0700

    hide menu

--- src/background.js
+++ src/background.js
@@ -19,7 +19,7 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    // autoHideMenuBar: true,
+    autoHideMenuBar: true,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
